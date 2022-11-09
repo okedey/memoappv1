aef49c48910a6d3e3554018e94ee78df6e312911
okedey <97540020+okedey@users.noreply.github.com>
2022-11-10 00:08:49 +0900

    second commit

--- original_memo/settings.py
+++ original_memo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-q@2=tphl!97a3n@3g_e+t^1gei+2_2^a)6!2jd#7r6r25l+f2a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,13 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATICFILES_DIRS = (
-    [
-        os.path.join(BASE_DIR, "static"),
-    ]
-)
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
