1ffd22829174f8a466e7ab59e8d8413244a5717b
okedey <97540020+okedey@users.noreply.github.com>
2022-11-10 11:53:30 +0900

    second commit

--- original_memo/settings.py
+++ original_memo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q@2=tphl!97a3n@3g_e+t^1gei+2_2^a)6!2jd#7r6r25l+f2a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
